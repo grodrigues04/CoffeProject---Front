cfa58f7f76139829fca1d9e1c27b4b696b6c861a
Gustavo Bernardo Rodrigues <gurodrigues.b@gmail.com>
2024-11-15 15:50:34 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Tasks from './components/Tasks'
+
+function App() {
+  //state: quando vc altera o state, vc atualiza o componente(a tela)
+        //valor atual, funcao valor que vai mudar o valor
+  //const [message, setMessage] = useState()//Dentro desse useState colocamos o valor inicial da variavel message
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default App
