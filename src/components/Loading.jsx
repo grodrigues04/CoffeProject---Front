16cdf9dfba4cc4a4004214883744cf9a1b7b71b0
Gustavo Bernardo Rodrigues <gurodrigues.b@gmail.com>
2024-11-25 17:54:00 -0300

    feat: adicao de componente de carregamento

---
+++ src/components/Loading.jsx
@@ -0,0 +1,12 @@
+import styles from "../assets/styles/loading.module.css"
+function Loading(){
+    return(
+        <>
+            <div className={styles.main_div}>
+                <h2>Carregando seus dados...</h2>
+                <div className={styles.loader}></div>
+            </div>
+        </>
+    )
+}
+export default Loading;
