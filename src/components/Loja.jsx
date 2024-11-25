5d85f4231d35d173b376ba03ecbbc9797caa54a9
Gustavo Bernardo Rodrigues <gurodrigues.b@gmail.com>
2024-11-25 17:53:14 -0300

    feat: retorno a loja

--- src/components/Loja.jsx
+++ src/components/Loja.jsx
@@ -2,6 +2,8 @@
 import styles from "../assets/styles/loja.module.css"
 import { Link } from 'react-router-dom'
 import { useEffect, useState } from "react";
+import Loading from "./Loading";
+import Error from "./Error";
 import Header from "./Header";
 function Loja(){
     const [cafes, setCafes] = useState([])
@@ -9,51 +11,77 @@ function Loja(){
     const [error, setError] = useState(false)
     useEffect(()=>{
         const fetchData = async () =>{
-            const response = await fetch("http://localhost:8000")
-            .then((res)=>{
-                return res.json();
-            })
-            .then((data)=>{
-                console.log(data)
-                setCafes(data)
-            })
-            .catch((error)=>{
+            try{
+                const response = await fetch("http://localhost:8000")
+                if(response.ok){
+                    let jsonData = await response.json()
+                    console.log(jsonData)
+                    setCafes(jsonData)
+                    setHtml(true)
+                }
+            }
+            catch(error){
+                console.log("Requisicao falhou")
                 setCafes(error)
-            })
+                setError(true)
+            }
         }
 
         fetchData()
     }, [])
 
-    return(
-        <>
-            <Header
-            home="../"
-            loja="../loja"
-            novocafe="../novocafe"
-            cafes="../cafes"
-            /> 
-            <section className={styles.recipe_intro}>
-                <h1 className={styles.section_main_text}>Descubra novas receitas deliciosas</h1>
-            </section>
+    if(error){
+        return(
+            <>
+                <Header
+                home="../"
+                loja="../loja"
+                novocafe="../novocafe"
+                cafes="../cafes"
+                />
+                <Error/>
+            </>
+        )
+    }
 
-            <section className={styles.recipe_grid}>
-                <article className={styles.recipe_card}>
-                    {cafes.map((item) => (
-                        <div key={item.id} className={styles.cafe_container}>
-                            <h3>{item.nome_cafe}</h3>
-                            <img src={item.img} alt={`img de ${item.nome_cafe}`} className={styles.img_config}/>
-                            <div className={styles.cafe_container_infos}>
-                                <p>Preço: R${item.preco}</p>
-                                <p>Vencimento: {item.ano_vencimento}</p>
+
+    if(!html){
+        return(
+            <Loading/>
+        )
+    }
+
+    if(html){
+        return(
+            <>
+                <Header
+                home="../"
+                loja="../loja"
+                novocafe="../novocafe"
+                cafes="../cafes"
+                /> 
+                <section className={styles.recipe_intro}>
+                    <h1 className={styles.section_main_text}>Descubra novas receitas deliciosas</h1>
+                </section>
+
+                <section className={styles.recipe_grid}>
+                    <article className={styles.recipe_card}>
+                        {cafes.map((item) => (
+                            <div key={item.id} className={styles.cafe_container}>
+                                <h3>{item.nome_cafe}</h3>
+                                <img src={item.img} alt={`img de ${item.nome_cafe}`} className={styles.img_config}/>
+                                <div className={styles.cafe_container_infos}>
+                                    <p>Preço: R${item.preco}</p>
+                                    <p>Vencimento: {item.ano_vencimento}</p>
+                                </div>
                             </div>
-                        </div>
-                    ))}
-                </article>
+                        ))}
+                    </article>
 
-            </section>
-        </>
-    )
+                </section>
+            </>
+        )
+    }
 }
 
 export default Loja;
